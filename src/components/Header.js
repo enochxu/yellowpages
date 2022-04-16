d4b27b91648e24d6f419acfe8e934b8ba14505dd
Enoch Xu <enoch.t.xu@gmail.com>
2022-04-15 23:21:17 -0700

    add top rated

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Navbar, Nav, Container, Button } from 'react-bootstrap';
+import { Navbar, Nav, Container, Button, Form, FormControl } from 'react-bootstrap';
 import { Link } from 'gatsby';
 import logo from '../images/logo1.svg';
 import './Header.css';
@@ -7,18 +7,26 @@ import './Header.css';
 const Header = () => {
   return (
     <Navbar collapseOnSelect expand="md">
-      <Container>
-        <Navbar.Brand href="/">
-          <img id="logo" src={logo} height="100" alt="yellowpages" />
-        </Navbar.Brand>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="/howitworks">How it works</Nav.Link>
-            <Nav.Link href="/contact">Contact</Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
+      <Navbar.Brand href="/">
+        <img id="logo" src={logo} height="100" alt="yellowpages" />
+      </Navbar.Brand>
+      <Nav aria-controls="responsive-navbar-nav">
+        <Form className="d-flex">
+          <FormControl
+            type="search"
+            placeholder="Search"
+            className="me-2"
+            aria-label="Search"
+          />
+          <Button variant="outline-success">Search</Button>
+        </Form>
+      </Nav>
+      {/* <Navbar.Collapse id="responsive-navbar-nav"> */}
+        {/* <Nav className="me-auto">
+          <Nav.Link href="/howitworks">How it works</Nav.Link>
+          <Nav.Link href="/contact">Contact</Nav.Link>
+        </Nav> */}
+      {/* </Navbar.Collapse> */}
     </Navbar>
   );
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,14 @@
 import * as React from "react"
+import "./index.css"
 
 // markup
 const IndexPage = () => {
   return (
     <main>
       <title>yellowpages</title>
+      <div className="featured">
+        <h1>Top Rated For You</h1>
+      </div>
     </main>
   )
 }
