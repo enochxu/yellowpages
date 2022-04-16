87d1f411c311d2900d9a6d72fd7ce32572e94a4a
Paul Zhang <paulz@ucla.edu>
2022-04-16 13:40:29 -0700

    Show restroom type in user reviews

--- src/components/Restroom.js
+++ src/components/Restroom.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import Rating from '@mui/material/Rating';
-import BookmarkAddIcon from '@mui/icons-material/BookmarkAdd';
-import Button from '@mui/material/Button';
-import ReviewCard from './ReviewCard';
-import "./Restroom.css"
+import React from "react";
+import Rating from "@mui/material/Rating";
+import BookmarkAddIcon from "@mui/icons-material/BookmarkAdd";
+import Button from "@mui/material/Button";
+import ReviewCard from "./ReviewCard";
+import "./Restroom.css";
 
 function Restroom(name, gender) {
-    const [value, setValue] = React.useState(4);
+  const [value, setValue] = React.useState(4);
 
-    /*
+  /*
         To-do:
             - Implement designs from figma first
             - Gender Icon
@@ -18,47 +18,55 @@ function Restroom(name, gender) {
                 (sinks, toilets, urinals, showers, etc.)
     */
 
-    return (
-        <>
-        <div className='restroom'>
-            <div className='header'>
-                <h2>Name of Restroom</h2>
-                <div className='sub'>
-                    <div className='rating'>
-                        <p>4.0/5</p>
-                        <Rating name="read-only" value={value} readOnly />
-                    </div>
-                    <div className='save'>
-                        <BookmarkAddIcon />
-                    </div>
-                </div>
+  return (
+    <>
+      <div className="restroom">
+        <div className="header">
+          <h2>Name of Restroom</h2>
+          <div className="sub">
+            <div className="rating">
+              <p>4.0/5</p>
+              <Rating name="read-only" value={value} readOnly />
             </div>
-            <div className='photo-section'>
-                <h3>Photos</h3>
-                <div className='photo-gallery'>
-                    <img src='https://i.ibb.co/2c90ZNw/ackerman-bathroom.jpg' className='photos' alt='bathroom'/>
-                    <img className='photos' alt='bathroom'/>
-                    <img className='photos' alt='bathroom'/>
-                    <img className='photos' alt='bathroom'/>
-                    <img className='photos' alt='bathroom'/>
-                    <img className='photos' alt='bathroom'/>
-                </div>
+            <div className="save">
+              <BookmarkAddIcon />
             </div>
+          </div>
         </div>
-        <div className='reviews'>
-            <h3 className='review-header'>Reviews</h3>
-            <div className="reviews-section">
-                <div className='review-bubbles'>
-                    <ReviewCard name='Ackerman' rating='5' reviewText='#blessed' />
-                    
-                </div>
-                <Button variant="contained" size="medium">
-                    More
-                </Button>
-            </div>
+        <div className="photo-section">
+          <h3>Photos</h3>
+          <div className="photo-gallery">
+            <img
+              src="https://i.ibb.co/2c90ZNw/ackerman-bathroom.jpg"
+              className="photos"
+              alt="bathroom"
+            />
+            <img className="photos" alt="bathroom" />
+            <img className="photos" alt="bathroom" />
+            <img className="photos" alt="bathroom" />
+            <img className="photos" alt="bathroom" />
+            <img className="photos" alt="bathroom" />
+          </div>
         </div>
-        </>
-    )
+      </div>
+      <div className="reviews">
+        <h3 className="review-header">Reviews</h3>
+        <div className="reviews-section">
+          <div className="review-bubbles">
+            <ReviewCard
+              name="Ackerman"
+              type="women"
+              rating="5"
+              reviewText="#blessed"
+            />
+          </div>
+          <Button variant="contained" size="medium">
+            More
+          </Button>
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default Restroom
+export default Restroom;
--- src/components/ReviewCard.js
+++ src/components/ReviewCard.js
@@ -1,13 +1,22 @@
 import React from "react";
 import { Rating } from "@mui/material";
 import { Card, Row } from "react-bootstrap";
+import "bootstrap-icons/font/bootstrap-icons.css";
 import "./ReviewCard.css";
 
-const ReviewCard = ({ name, rating, reviewText }) => {
+const restroomTypes = new Map([
+  ["gender-neutral", "bi-gender-ambiguous"],
+  ["women", "bi-gender-female"],
+  ["men", "bi-gender-male"],
+]);
+
+const ReviewCard = ({ name, type, rating, reviewText }) => {
   return (
-    <Card className="review-card" style={{ width: "40%" }}>
+    <Card className="review-card" type={type} style={{ width: "40%" }}>
       <Card.Body>
-        <Card.Title>{name}</Card.Title>
+        <Card.Title>
+          {name} <i class={restroomTypes.get(type)}></i>
+        </Card.Title>
         <Rating name="read-only" value={rating} readOnly />
         <Card.Text>{reviewText}</Card.Text>
       </Card.Body>
