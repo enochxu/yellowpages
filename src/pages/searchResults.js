4214985641fff56a6c00b6b2c3e26dbb0ac9bc33
Enoch Xu <enoch.t.xu@gmail.com>
2022-04-16 17:59:13 -0700

    add urls

--- src/pages/searchResults.js
+++ src/pages/searchResults.js
@@ -13,9 +13,9 @@ const ResultsPage = ({ location }) => {
       </div> */}
       <div className="results-grid">
         <Card name="Ackerman Grand Ballroom" ratingValue="4.8"  image="https://i.ibb.co/2c90ZNw/ackerman-bathroom.jpg" distance="0.0 mi away" url="/samples/ackerman"/>
-        <Card name="Ackerman Level 1" ratingValue="1.0" image="https://i.ibb.co/GRmDHyy/ackerman1entrancemens.jpg" distance="1.0 mi away"/>
-        <Card name="Powell Library Basement" ratingValue="3.2" image="https://i.ibb.co/SxLzd7m/powellstalls.jpg" distance="0.2 mi away"/>
-        <Card name="Kaplan Level 1" ratingValue="5.0" image="https://i.ibb.co/6sKbXx3/kaplan1stallswomen.jpg" distance="0.2 mi away"/>
+        <Card name="Ackerman Level 1" ratingValue="1.0" image="https://i.ibb.co/GRmDHyy/ackerman1entrancemens.jpg" distance="1.0 mi away" url="/samples/ackermanl1"/>
+        <Card name="Powell Library Basement" ratingValue="3.2" image="https://i.ibb.co/SxLzd7m/powellstalls.jpg" distance="0.2 mi away" url="/samples/powellBasement" />
+        <Card name="Kaplan Level 1" ratingValue="5.0" image="https://i.ibb.co/6sKbXx3/kaplan1stallswomen.jpg" distance="0.2 mi away" url="/samples/kaplan1" />
       </div>
     </div>
   )
