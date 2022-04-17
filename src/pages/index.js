a05f6ca01b2de1d23d72ed0c5eeb6f2dc6a24f5a
Paul Zhang <paulz@ucla.edu>
2022-04-16 22:06:54 -0700

    Round average rating and manually update ratings

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
-import * as React from "react"
-import "./index.css"
-import Card from "../components/Card.js"
+import * as React from "react";
+import "./index.css";
+import Card from "../components/Card.js";
 import "react-multi-carousel/lib/styles.css";
 
 // markup
@@ -11,15 +11,45 @@ const IndexPage = () => {
       <div className="featured">
         <h1>Top Rated For You</h1>
         <div className="restroom-grid">
-          <Card name="Ackerman Grand Ballroom" ratingValue="4.8"  image="https://i.ibb.co/2c90ZNw/ackerman-bathroom.jpg" distance="0.0 mi away" url="/samples/ackerman" />
-          <Card name="Ackerman Level 1" ratingValue="4.2"  image="https://i.ibb.co/GRmDHyy/ackerman1entrancemens.jpg" distance="0.0 mi away" url="/samples/ackerman1" />
-          <Card name="Olympic Hall 9" ratingValue="5.0" image="https://i.ibb.co/4gSBtM4/olympic-sinks.jpg" distance="0.7 mi away" url="/samples/olympic9" />
-          <Card name="Powell Library Basement" ratingValue="3.2" image="https://i.ibb.co/SxLzd7m/powellstalls.jpg" distance="0.2 mi away" url="/samples/powellBasement" />
-          <Card name="Kaplan Level 1" ratingValue="1.0" image="https://i.ibb.co/6sKbXx3/kaplan1stallswomen.jpg" distance="0.2 mi away" url="/samples/kaplan1" />
+          <Card
+            name="Ackerman Grand Ballroom"
+            ratingValue="4.7"
+            image="https://i.ibb.co/2c90ZNw/ackerman-bathroom.jpg"
+            distance="0.0 mi away"
+            url="/samples/ackerman"
+          />
+          <Card
+            name="Ackerman Level 1"
+            ratingValue="3.5"
+            image="https://i.ibb.co/GRmDHyy/ackerman1entrancemens.jpg"
+            distance="0.0 mi away"
+            url="/samples/ackerman1"
+          />
+          <Card
+            name="Olympic Hall 9"
+            ratingValue="3.3"
+            image="https://i.ibb.co/4gSBtM4/olympic-sinks.jpg"
+            distance="0.7 mi away"
+            url="/samples/olympic9"
+          />
+          <Card
+            name="Powell Library Basement"
+            ratingValue="4.7"
+            image="https://i.ibb.co/SxLzd7m/powellstalls.jpg"
+            distance="0.2 mi away"
+            url="/samples/powellBasement"
+          />
+          <Card
+            name="Kaplan Level 1"
+            ratingValue="4.7"
+            image="https://i.ibb.co/6sKbXx3/kaplan1stallswomen.jpg"
+            distance="0.2 mi away"
+            url="/samples/kaplan1"
+          />
         </div>
       </div>
     </main>
-  )
-}
+  );
+};
 
-export default IndexPage
+export default IndexPage;
--- src/pages/samples/ackerman.js
+++ src/pages/samples/ackerman.js
@@ -1,58 +1,71 @@
-import React from "react"
-import Restroom from "../../components/Restroom"
-import AddReview from "../../components/Addreview"
-
+import React from "react";
+import Restroom from "../../components/Restroom";
+import AddReview from "../../components/Addreview";
 
 const Ackerman = () => {
-
   const reviews = [
     {
-      'name': 'Ackerman Grand Ballroom',
-      'rating': '3.0',
-      'reviewText': 'Honestly pretty bleh. Don’t let the number of stalls fool you. The lighting is pretty uneven so some stalls are super dark while others are really bright. On top of that, it took me a solid minute to figure out how to lock the stall because the locks require a lot of rotations before the tongue lock pops out.',
-      'type': 'women',
+      name: "Ackerman Grand Ballroom",
+      rating: "3.0",
+      reviewText:
+        "Honestly pretty bleh. Don’t let the number of stalls fool you. The lighting is pretty uneven so some stalls are super dark while others are really bright. On top of that, it took me a solid minute to figure out how to lock the stall because the locks require a lot of rotations before the tongue lock pops out.",
+      type: "women",
     },
     {
-      'name': 'Ackerman Grand Ballroom',
-      'rating': '5.0',
-      'reviewText': 'This bathroom has more stalls than my instagram followers. Potential party venue.',
-      'type': 'men',
+      name: "Ackerman Grand Ballroom",
+      rating: "5.0",
+      reviewText:
+        "This bathroom has more stalls than my instagram followers. Potential party venue.",
+      type: "men",
     },
     {
-      'name': 'Ackerman Grand Ballroom',
-      'rating': '5.0',
-      'reviewText': 'The capacity of these bathrooms is actually incredible. Perfect for students and families that are touring, but also big enough that it doesn’t annoy us students who actually go here.',
-      'type': 'women',
+      name: "Ackerman Grand Ballroom",
+      rating: "5.0",
+      reviewText:
+        "The capacity of these bathrooms is actually incredible. Perfect for students and families that are touring, but also big enough that it doesn’t annoy us students who actually go here.",
+      type: "women",
     },
     {
-      'name': 'Ackerman Grand Ballroom',
-      'rating': '5.0',
-      'reviewText': 'This bathroom has a diaper changing station! It’s a very large and roomy bathroom which was super helpful. What a life saver.',
-      'type': 'women',
+      name: "Ackerman Grand Ballroom",
+      rating: "5.0",
+      reviewText:
+        "This bathroom has a diaper changing station! It’s a very large and roomy bathroom which was super helpful. What a life saver.",
+      type: "women",
     },
     {
-      'name': 'Ackerman Grand Ballroom',
-      'rating': '5.0',
-      'reviewText': 'Grand, Mesmerizing, life changing! ',
-      'type': 'men',
-    }
-  ]
+      name: "Ackerman Grand Ballroom",
+      rating: "5.0",
+      reviewText: "Grand, Mesmerizing, life changing! ",
+      type: "men",
+    },
+    {
+      name: "Ackerman Grand Ballroom",
+      rating: "5.0",
+      reviewText: "Having a toilet to myself is the peak of defecatory luxury.",
+      type: "gender-neutral",
+    },
+  ];
 
   const images = [
-      'https://i.ibb.co/mhjrN8m/ackerman-bathroom.jpg',
-      'https://i.ibb.co/26dh89h/ackerman2.jpg',
-      'https://i.ibb.co/Yfg9H3B/ackermangrandmenstalls.jpg',
-      'https://i.ibb.co/3dgmFLS/womensackermangrandentrance.jpg',
-      'https://i.ibb.co/Fb48wcs/womensackermengrandsinksdiaper.jpg',
+    "https://i.ibb.co/mhjrN8m/ackerman-bathroom.jpg",
+    "https://i.ibb.co/26dh89h/ackerman2.jpg",
+    "https://i.ibb.co/Yfg9H3B/ackermangrandmenstalls.jpg",
+    "https://i.ibb.co/3dgmFLS/womensackermangrandentrance.jpg",
+    "https://i.ibb.co/Fb48wcs/womensackermengrandsinksdiaper.jpg",
+    "https://ibb.co/44cb8RS", // gender-neutral toilet
+  ];
 
-  ]
+  return (
+    <div className="ackerman">
+      <Restroom
+        name="Ackerman Grand Ballroom"
+        rating="3"
+        reviews={reviews}
+        images={images}
+      />
+      <AddReview />
+    </div>
+  );
+};
 
-    return (
-      <div className="ackerman">
-        <Restroom name='Ackerman Grand Ballroom' rating='3' reviews={reviews} images={images} />
-        <AddReview />
-      </div>
-    )
-  }
-  
-  export default Ackerman;
+export default Ackerman;
