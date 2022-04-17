3ff3e65d7f56d46fb5f525a5903035bcb176bc9b
Ryan Kim <ryanswkim2003@gmail.com>
2022-04-16 22:13:46 -0700

    final

--- src/pages/samples/ackerman.js
+++ src/pages/samples/ackerman.js
@@ -3,53 +3,53 @@ import Restroom from "../../components/Restroom"
 import Rating from '@mui/material/Rating';
 import "../../components/AddReview.css"
 import { Button } from "react-bootstrap";
-// import AddReview from "../../components/Addreview"
 
+const NAME = "Ackerman Grand Ballroom"
+const images = [
+  'https://i.ibb.co/mhjrN8m/ackerman-bathroom.jpg',
+  'https://i.ibb.co/26dh89h/ackerman2.jpg',
+  'https://i.ibb.co/Yfg9H3B/ackermangrandmenstalls.jpg',
+  'https://i.ibb.co/3dgmFLS/womensackermangrandentrance.jpg',
+  'https://i.ibb.co/Fb48wcs/womensackermengrandsinksdiaper.jpg',
+
+]
+const DATA = [
+  {
+    'name': NAME,
+    'rating': '3.0',
+    'reviewText': 'Honestly pretty bleh. Don’t let the number of stalls fool you. The lighting is pretty uneven so some stalls are super dark while others are really bright. On top of that, it took me a solid minute to figure out how to lock the stall because the locks require a lot of rotations before the tongue lock pops out.',
+    'type': 'women',
+  },
+  {
+    'name': NAME,
+    'rating': '5.0',
+    'reviewText': 'This bathroom has more stalls than my instagram followers. Potential party venue.',
+    'type': 'men',
+  },
+  {
+    'name': NAME,
+    'rating': '5.0',
+    'reviewText': 'The capacity of these bathrooms is actually incredible. Perfect for students and families that are touring, but also big enough that it doesn’t annoy us students who actually go here.',
+    'type': 'women',
+  },
+  {
+    'name': NAME,
+    'rating': '5.0',
+    'reviewText': 'This bathroom has a diaper changing station! It’s a very large and roomy bathroom which was super helpful. What a life saver.',
+    'type': 'women',
+  },
+  {
+    'name': NAME,
+    'rating': '5.0',
+    'reviewText': 'Grand, Mesmerizing, life changing! ',
+    'type': 'men',
+  }
+]
 
 const Ackerman = () => {
 
   const [trigger, setTrigger] = useState(true);
-  const [reviews, setReviews] = useState([
-    {
-      'name': 'Ackerman Grand Ballroom',
-      'rating': '3.0',
-      'reviewText': 'Honestly pretty bleh. Don’t let the number of stalls fool you. The lighting is pretty uneven so some stalls are super dark while others are really bright. On top of that, it took me a solid minute to figure out how to lock the stall because the locks require a lot of rotations before the tongue lock pops out.',
-      'type': 'women',
-    },
-    {
-      'name': 'Ackerman Grand Ballroom',
-      'rating': '5.0',
-      'reviewText': 'This bathroom has more stalls than my instagram followers. Potential party venue.',
-      'type': 'men',
-    },
-    {
-      'name': 'Ackerman Grand Ballroom',
-      'rating': '5.0',
-      'reviewText': 'The capacity of these bathrooms is actually incredible. Perfect for students and families that are touring, but also big enough that it doesn’t annoy us students who actually go here.',
-      'type': 'women',
-    },
-    {
-      'name': 'Ackerman Grand Ballroom',
-      'rating': '5.0',
-      'reviewText': 'This bathroom has a diaper changing station! It’s a very large and roomy bathroom which was super helpful. What a life saver.',
-      'type': 'women',
-    },
-    {
-      'name': 'Ackerman Grand Ballroom',
-      'rating': '5.0',
-      'reviewText': 'Grand, Mesmerizing, life changing! ',
-      'type': 'men',
-    }
-  ])
-
-  const images = [
-      'https://i.ibb.co/mhjrN8m/ackerman-bathroom.jpg',
-      'https://i.ibb.co/26dh89h/ackerman2.jpg',
-      'https://i.ibb.co/Yfg9H3B/ackermangrandmenstalls.jpg',
-      'https://i.ibb.co/3dgmFLS/womensackermangrandentrance.jpg',
-      'https://i.ibb.co/Fb48wcs/womensackermengrandsinksdiaper.jpg',
-
-  ]
+  const [reviews, setReviews] = useState(DATA)
 
   function addReview(name, rating, reviewText, type){
     const temp = reviews;
@@ -75,7 +75,7 @@ const Ackerman = () => {
       }
       evt.preventDefault();
       
-      addReview("Ackerman Grand Ballroom", rating.toString(), comment, gender);
+      addReview(NAME, rating.toString(), comment, gender);
       setComment("");
       setGender("");
       setRating(0);
@@ -113,7 +113,7 @@ const Ackerman = () => {
 
     return (
       <div className="ackerman">
-        <Restroom name='Ackerman Grand Ballroom' rating='3' reviews={reviews} images={images} />
+        <Restroom name={NAME} rating='3' reviews={reviews} images={images} />
         <AddReview />
       </div>
     )
