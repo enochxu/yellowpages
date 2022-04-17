e87ba8a87eae6777aa4650c6e53bf09552c4d1bd
Enoch Xu <enoch.t.xu@gmail.com>
2022-04-16 22:34:21 -0700

    fix url

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,7 +12,7 @@ const IndexPage = () => {
         <h1>Top Rated For You</h1>
         <div className="restroom-grid">
           <Card name="Ackerman Grand Ballroom" ratingValue="4.7"  image="https://i.ibb.co/2c90ZNw/ackerman-bathroom.jpg" distance="0.0 mi away" url="/samples/ackerman" />
-          <Card name="Ackerman Level 1" ratingValue="3.5"  image="https://i.ibb.co/GRmDHyy/ackerman1entrancemens.jpg" distance="0.0 mi away" url="/samples/ackerman1" />
+          <Card name="Ackerman Level 1" ratingValue="3.5"  image="https://i.ibb.co/GRmDHyy/ackerman1entrancemens.jpg" distance="0.0 mi away" url="/samples/ackermanl1" />
           <Card name="Olympic Hall 9" ratingValue="3.3" image="https://i.ibb.co/4gSBtM4/olympic-sinks.jpg" distance="0.7 mi away" url="/samples/olympic9" />
           <Card name="Powell Library Basement" ratingValue="4.7" image="https://i.ibb.co/SxLzd7m/powellstalls.jpg" distance="0.2 mi away" url="/samples/powellBasement" />
           <Card name="Kaplan Level 1" ratingValue="4.7" image="https://i.ibb.co/6sKbXx3/kaplan1stallswomen.jpg" distance="0.2 mi away" url="/samples/kaplan1" />
