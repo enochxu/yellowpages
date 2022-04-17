383de7f64e24376cec82d4697903bf88aa02a981
Lydia Xu <lydiaxu10@gmail.com>
2022-04-16 19:58:28 -0700

    Add reviews to all the bathrooms

---
+++ src/pages/samples/ackermanl1.js
@@ -0,0 +1,26 @@
+const reviews = [
+    {
+      'name': 'Ackerman Level 1',
+      'rating': '3.0',
+      'reviewText': '',
+      'type': 'women',
+    },
+    {
+        'name': 'Ackerman Level 1',
+        'rating': '1.0',
+        'reviewText': 'Haven’t been to this one. I couldn’t find it.',
+        'type': 'men',
+    },
+    {
+      'name': 'Ackerman Level 1',
+      'rating': '5.0',
+      'reviewText': '',
+      'type': 'women',
+    },
+    {
+        'name': 'Ackerman Level 1',
+        'rating': '5.0',
+        'reviewText': 'Had a great night in this one!',
+        'type': 'men',
+      },
+  ]
---
+++ src/pages/samples/kaplan1.js
@@ -0,0 +1,20 @@
+const reviews = [
+    {
+      'name': 'Kaplan Level 1',
+      'rating': '5.0',
+      'reviewText': 'Why can’t south campus bathrooms be more like north campus bathrooms :(',
+      'type': 'women',
+    },
+    {
+      'name': 'Kaplan Level 1',
+      'rating': '4.0',
+      'reviewText': 'Pretty clean, but nothing outstanding so I’d give a solid 4. ',
+      'type': 'women',
+    },
+    {
+      'name': 'Kaplan Level 1',
+      'rating': '5.0',
+      'reviewText': '',
+      'type': 'men',
+    }
+  ]
